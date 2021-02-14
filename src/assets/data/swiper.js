2793de971170d2f36f980c078753bfc812b44708
Mac <mac@MacdeMac.local>
2021-02-14 17:42:04 +0800

    [NEW] 项目初始化

---
+++ src/assets/data/swiper.js
@@ -0,0 +1,18 @@
+//轮播图的数据
+const swiperList = [
+    // {picImg:''},
+        // {picImg: 'https://imgessl.kugou.com/commendpic/20210207/20210207175602629621.jpg'},
+
+    // {picImg: require('@/assets/img/swiper/1.jpg')},
+    // {picImg: require('@/assets/img/swiper/2.jpg')},
+    // {picImg: require('@/assets/img/swiper/3.jpg')},
+    // {picImg: require('@/assets/img/swiper/4.jpg')},
+    // {picImg: require('@/assets/img/swiper/5.jpg')},
+    // {picImg: require('@/assets/img/swiper/6.jpg')},
+    // {picImg: require('@/assets/img/swiper/7.jpg')},
+    // {picImg: require('@/assets/img/swiper/8.jpg')}    
+]
+
+export {
+    swiperList
+}
